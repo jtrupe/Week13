663d7baa4f0fdd3e9180eac49149ff8b3dde778e
jtrupe <53407584+jtrupe@users.noreply.github.com>
2021-11-22 14:12:32 -0800

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,50 @@
+﻿/// Week 13 Lab 1
+///
+/// @author: Julian Trupe
+/// Date:  November 19, 2021
+///
+/// Problem Statement: Create a test program that implements optional parameters, named parameters, and variable parameters
+//
+/// Overall Plan:
+/// 1) Write speak statement with optional text parameter
+/// 2) Write printFirst function
+/// 3) Write multiAdder function which takes variable parameters and adds them
+/// 4) Test each function in the main method, use named parameters for printFirst
+/// 
+
+using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            speak();
+            speak("Hello!");
+            printFirst(second: "nothing", first: "FIRST!");
+            Console.WriteLine(multiAdder(1, 2, 3, 4, 5));
+        }
+
+        public static void speak(string text= "default statement")
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void printFirst(string first, string second)
+        {
+            Console.WriteLine(first);
+        }
+
+        public static int multiAdder(params int[] nums)
+        {
+            int sum = 0;
+            foreach(int j in nums)
+            {
+                sum += j;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,82 @@
+﻿/// Week 13 Project 1
+///
+/// @author: Julian Trupe
+/// Date:  November 19, 2021
+///
+/// Problem Statement: Allow the user to type a name, tell user the rank of the name on the girls/boys lists
+//
+/// Overall Plan:
+/// 1) Define an object CountRank with properties count and rank as shows above
+/// 2) Define BoyNames and GirlNames, two dictionaries
+/// 3) Read boynames.txt and girlnames.txt into dictionaries where key= person's name value = CountRank
+/// 4) Prompt user to enter a name
+/// 5) User ContainsKey to check each Dictionary 
+/// 6) Display results message to the console 
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rankM = 1;
+            int rankF = 1;
+            Dictionary<string, CountRank> BoyNames = new Dictionary<string, CountRank>();
+            Dictionary<string, CountRank> GirlNames = new Dictionary<string, CountRank>();
+            foreach(string line in File.ReadLines("D:\\MiraCosta Schoolwork\\CS 155\\Week13\\Project1\\boynames.txt"))
+            {
+                string[] temp = line.Split();
+                CountRank tempCR = new CountRank(Convert.ToInt32(temp[1]), rankM);
+                BoyNames.Add(temp[0], tempCR);
+                rankM++;
+
+                //Testing fileread to dictionary works properly
+                //Console.WriteLine(temp[0] + " " + BoyNames.ContainsKey(temp[0]) + " " + BoyNames[temp[0]].Count + " " + BoyNames[temp[0]].Rank);
+            }
+            foreach (string line in File.ReadLines("D:\\MiraCosta Schoolwork\\CS 155\\Week13\\Project1\\girlnames.txt"))
+            {
+                string[] temp = line.Split();
+                CountRank tempCR = new CountRank(Convert.ToInt32(temp[1]), rankF);
+                GirlNames.Add(temp[0], tempCR);
+                rankF++;
+
+                //Testing fileread to dictionary works properly
+                //Console.WriteLine(temp[0] + " " + GirlNames.ContainsKey(temp[0]) + " " + GirlNames[temp[0]].Count + " " + GirlNames[temp[0]].Rank);
+            }
+            Console.WriteLine("Please enter a name and will tell you its rank among boys and girls");
+            string inputName = Console.ReadLine();
+            if (BoyNames.ContainsKey(inputName))
+            {
+                Console.WriteLine(inputName + " is ranked " + BoyNames[inputName].Rank + " among boys with " + BoyNames[inputName].Count + " namings.");
+            }
+            else
+            {
+                Console.WriteLine(inputName + " is not ranked among the top 1000 boy names.");
+            }
+            if (GirlNames.ContainsKey(inputName))
+            {
+                Console.WriteLine(inputName + " is ranked " + GirlNames[inputName].Rank + " among girls with " + GirlNames[inputName].Count + " namings.");
+            }
+            else
+            {
+                Console.WriteLine(inputName + " is not ranked among the top 1000 girl names.");
+            }
+        }
+    }
+
+    class CountRank
+    {
+        public CountRank(int ct, int rk)
+        {
+            Count = ct;
+            Rank = rk;
+        }
+
+        public int Count{get; set;}
+        public int Rank {get; set;}
+    }
+}
